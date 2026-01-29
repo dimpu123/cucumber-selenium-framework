a8224dfc85a37a1d69c3040bc0a0542cf2a6d18a
dimpu123 <saisanthosh416@gmail.com>
2026-01-29 09:00:06 +0530

    third push

--- src/test/java/stepDefinitions/HomeSteps.java
+++ src/test/java/stepDefinitions/HomeSteps.java
@@ -9,7 +9,7 @@ public class HomeSteps extends BaseTest {
 
 	HomePage homePage;
 
-	@Given("check urlssp")
+	@Given("check urlsspp")
 	public void check_url() {
 		homePage = new HomePage(driver);
 		homePage.openHomePage();
--- src/test/resources/features/HRM.feature
+++ src/test/resources/features/HRM.feature
@@ -2,6 +2,6 @@ Feature: Testing new application
 
   @start
   Scenario: User is able to open the home page
-    Given check urlssp
+    Given check urlsspp
     When redirects the link
     Then open the home page
